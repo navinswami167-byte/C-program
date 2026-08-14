8adf87477e51f9effcbfbbc9a3ce2e73ffdb1935
navinswami167-byte <navinswami167@gmail.com>
2026-08-14 19:47:07 +0530

    Addition of two number

---
+++ c/Add_two_number.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+
+    float a,b,sum;
+    printf("Enter your frist number : ");
+
+    scanf("%f",&a);
+    printf("Enter your second number : ");
+    scanf("%f",&b);
+
+    sum = a+b ;
+
+   printf("%f",sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
