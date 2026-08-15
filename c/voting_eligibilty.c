f371092928b444b4bedfd1e77ab27e59281c9215
navinswami167-byte <navinswami167@gmail.com>
2026-08-15 20:42:26 +0530

    checking eligibilty for voting

---
+++ c/voting_eligibilty.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{ int a;
+    printf("Enter your age :");
+    scanf("%d",&a);
+    if (a>=18)
+
+    {  
+      printf("You are eligible ");
+    }
+ else 
+ {
+    printf("You are not eligible ");
+
+ }
+
+
+
+
+}
