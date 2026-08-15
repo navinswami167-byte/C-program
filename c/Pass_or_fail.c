f01b70b1e63d5d5214aad5c31ce68ddd76fd99e1
navinswami167-byte <navinswami167@gmail.com>
2026-08-15 21:34:17 +0530

    Check for pass or fail

---
+++ c/Pass_or_fail.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{    int a;
+    printf("Enter your score :");
+    scanf("%d",&a);
+
+    if (a>=40)
+   { printf("Pass");}
+
+    else 
+    {printf("Fail");}
+
+
+
+
+
+
+}
