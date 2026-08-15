964ac116fd5db0ccfca3637e73fd6a61efeac8b5
navinswami167-byte <navinswami167@gmail.com>
2026-08-15 20:52:59 +0530

    Largest of two number

---
+++ c/largest_of_two_num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+
+    printf("Enter your first number : ");
+   scanf("%f",&a);
+   printf("Enter your second number : ");
+   scanf("%f",&b);
+   if (a>b)
+   {printf("%f",a);}
+else 
+{printf("%f",b);}
+
+
+}
