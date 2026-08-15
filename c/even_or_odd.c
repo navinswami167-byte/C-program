42098c2b73fc648743fb248836eb34437a256870
navinswami167-byte <navinswami167@gmail.com>
2026-08-15 20:30:33 +0530

    Even or odd

---
+++ c/even_or_odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   int a,b;
+   printf("Enter your number :");
+   scanf("%d",&a);
+   b = a % 2;
+
+   if(b == 0 )
+   {  printf("Your number is even ");
+
+   }
+
+   else 
+   {printf("Your number is odd ");}
+
+
+}
