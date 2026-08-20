a97627155a401cbf7b41297cd93dedd898f4c53f
navinswami167-byte <navinswami167@gmail.com>
2026-08-20 22:37:37 +0530

    Sum of n number

---
+++ c/Sum_of_n_number.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{ int i,n,sum;
+  i=1;
+  printf("Enter natural number for which you need sum :");
+  scanf("%d",&n);
+  while(i<=n)
+  {
+sum+=i;
+       i++;
+
+
+
+  }
+
+printf("Your sum is %d",sum);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
