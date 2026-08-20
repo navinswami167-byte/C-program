3c4f6cdb42fa75585228399ad730a5d81ad5214c
navinswami167-byte <navinswami167@gmail.com>
2026-08-20 23:09:44 +0530

    Table

---
+++ c/Table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+	int main()
+	{ int n,b;
+	 printf("Enter your number for table :");
+	scanf("%d",&n);
+	b=1;
+	while(b<=10)
+	{ 
+	   printf("%d\n",n*b);
+	   
+	 b++;
+	}
+	
+	}
